8435b093404e50ef51276fb5b2b17053f0bc0e42
Fabian Arlt <faarlt@microsoft.com>
2020-03-24 14:10:20 +0100

    Added 'MSO' to array

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 //the array contains all "Comp Code" achronyms, that will disapear
-var array = ["ISI", "SME", "AME", "PRH"];
+var array = ["ISI", "SME", "AME", "PRH", "MSO"];
 
 var tbl = document.body.getElementsByTagName('tbody');
 
